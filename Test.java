8dd3d1330ce1f9fe736159cbdcde207ebbb33241
pravrawa <praveen.25june@gmail.com>
2017-04-07 14:37:07 +0530

    SP-1 updated Test.java file

---
+++ Test.java
@@ -0,0 +1,20 @@
+package day1.examples;
+
+public class ExampleIf {
+
+
+	public static void main(String[] args) {
+
+		int x=440;
+		int y=220;
+		
+		if (x<y){
+			System.out.println("X is less than Y");
+		}else if (x==y){
+			System.out.println("X is Equal to Y");
+		}else {
+			System.out.println("X is greater than Y");
+		}
+	}
+
+}
